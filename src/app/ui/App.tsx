c25943ee33b9270c760291bc1ce7a4086e62ac85
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-09-15 16:37:53 +0300

    created rtk-query-music app, added routing

---
+++ src/app/ui/App.tsx
@@ -0,0 +1,11 @@
+import { Routing } from '@/common/routing/Routing';
+
+export const App = () => {
+  return (
+    <>
+      <Routing />
+    </>
+  );
+};
+
+export default App;
---
+++ src/common/routing/Routing.tsx
@@ -0,0 +1,24 @@
+import { MainPage } from '@/app/ui/MainPage/MainPage';
+import { ProfilePage } from '@/features/auth/ui/PropfilePage/ProfilePage';
+import { PlaylistsPage } from '@/features/playlists/ui/PlaylistsPage';
+import { TracksPage } from '@/features/tracks/ui/TracksPage';
+import { Route, Routes } from 'react-router';
+import { PageNotFound } from '../components/PageNotFound/PageNotFound';
+
+export const Path = {
+  Main: '/',
+  Playlists: '/playlists',
+  Tracks: '/tracks',
+  Profile: '/profile',
+  NotFound: '*',
+} as const;
+
+export const Routing = () => (
+  <Routes>
+    <Route path={Path.Main} element={<MainPage />} />
+    <Route path={Path.Playlists} element={<PlaylistsPage />} />
+    <Route path={Path.Tracks} element={<TracksPage />} />
+    <Route path={Path.Profile} element={<ProfilePage />} />
+    <Route path={Path.NotFound} element={<PageNotFound />} />
+  </Routes>
+);
