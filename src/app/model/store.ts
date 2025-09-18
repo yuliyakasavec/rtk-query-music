3056ff1b0c375a4c002e8e16f66ad47bee897bbe
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-09-18 23:58:44 +0300

    added Code Splitting, used injectEndpoints

--- src/app/model/store.ts
+++ src/app/model/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import { playlistsApi } from '@/features/playlists/api/playlistsApi.ts';
+import { baseApi } from '../api/baseApi';
 
 export const store = configureStore({
   reducer: {
-    [playlistsApi.reducerPath]: playlistsApi.reducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(playlistsApi.middleware),
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 setupListeners(store.dispatch);
--- src/features/playlists/api/playlistsApi.ts
+++ src/features/playlists/api/playlistsApi.ts
@@ -1,27 +1,12 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type {
   CreatePlaylistArgs,
   PlaylistData,
   PlaylistsResponse,
   UpdatePlaylistArgs,
 } from './playlistsApi.types';
+import { baseApi } from '@/app/api/baseApi';
 
-export const playlistsApi = createApi({
-  reducerPath: 'playlistsApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
-    },
-    prepareHeaders: (headers) => {
-      headers.set(
-        'Authorization',
-        `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
-      );
-      return headers;
-    },
-  }),
-  tagTypes: ['Playlist'],
+export const playlistsApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     fetchPlaylists: build.query<PlaylistsResponse, void>({
       query: () => {
