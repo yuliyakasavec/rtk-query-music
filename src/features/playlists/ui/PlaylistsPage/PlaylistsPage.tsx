e8246883e4771b9e49c7e0bcebd31bf36de99e62
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-10-05 18:45:32 +0300

    made Error Handling in PlaylistPage

--- src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
+++ src/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx
@@ -1,10 +1,11 @@
-import { useState, type ChangeEvent } from 'react';
+import { useEffect, useState, type ChangeEvent } from 'react';
 import { useFetchPlaylistsQuery } from '../../api/playlistsApi';
 import { CreatePlaylistForm } from './CreatePlaylistForm/CreatePlaylistForm';
 import s from './PlaylistsPage.module.css';
 import { useDebounceValue } from '@/common/hooks';
 import { Pagination } from '@/common/components';
 import { PlaylistsList } from './PlaylistList/PlaylistList';
+import { toast } from 'react-toastify';
 
 export const PlaylistsPage = () => {
   const [search, setSearch] = useState('');
@@ -14,11 +15,35 @@ export const PlaylistsPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(2);
 
-  const { data, isLoading } = useFetchPlaylistsQuery({
+  const { data, isLoading, error } = useFetchPlaylistsQuery({
     search: debounceSearch,
     pageNumber: currentPage,
     pageSize,
   });
+
+  useEffect(() => {
+    if (!error) return;
+    if ('status' in error) {
+      // FetchBaseQueryError
+      const errMsg =
+        'error' in error
+          ? error.error
+          : (
+              error.data as {
+                error: string;
+              }
+            ).error ||
+            (error.data as { message: string }).message ||
+            'Some error occurred';
+      toast(errMsg, { type: 'error', theme: 'colored' });
+    } else {
+      // SerializedError
+      toast(error.message || 'Some error occurred', {
+        type: 'error',
+        theme: 'colored',
+      });
+    }
+  }, [error]);
 
   const changePageSizeHandler = (size: number) => {
     setPageSize(size);
