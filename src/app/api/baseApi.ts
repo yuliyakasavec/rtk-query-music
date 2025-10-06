acd92028ced705054e49b8073c38acf54b1f5b95
Yuliya Kasavets <kosoveculia8@gmail.com>
2025-10-06 11:56:23 +0300

    Error handling: FETCH_ERROR, PARSING_ERROR, CUSTOM_ERROR, TIMEOUT_ERROR

--- src/app/api/baseApi.ts
+++ src/app/api/baseApi.ts
@@ -1,4 +1,8 @@
-import { isErrorWithProperty } from '@/common/utils';
+import {
+  isErrorWithDetailArray,
+  isErrorWithProperty,
+  trimToMaxLength,
+} from '@/common/utils';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { toast } from 'react-toastify';
 
@@ -23,6 +27,13 @@ export const baseApi = createApi({
 
     if (result.error) {
       switch (result.error.status) {
+        case 'FETCH_ERROR':
+        case 'PARSING_ERROR':
+        case 'CUSTOM_ERROR':
+        case 'TIMEOUT_ERROR':
+          toast(result.error.error, { type: 'error', theme: 'colored' });
+          break;
+
         case 404:
           if (isErrorWithProperty(result.error.data, 'error')) {
             toast(result.error.data.error, { type: 'error', theme: 'colored' });
@@ -35,6 +46,7 @@ export const baseApi = createApi({
 
           break;
 
+        case 401:
         case 429:
           if (isErrorWithProperty(result.error.data, 'message')) {
             toast(result.error.data.message, {
@@ -49,8 +61,30 @@ export const baseApi = createApi({
           }
           break;
 
+        case 400:
+        case 403:
+          if (isErrorWithDetailArray(result.error.data)) {
+            toast(trimToMaxLength(result.error.data.errors[0].detail), {
+              type: 'error',
+              theme: 'colored',
+            });
+          } else {
+            toast(JSON.stringify(result.error.data), {
+              type: 'error',
+              theme: 'colored',
+            });
+          }
+          break;
+
         default:
-          toast('Some error occurred', { type: 'error', theme: 'colored' });
+          if (result.error.status >= 500 && result.error.status < 600) {
+            toast('Server error occurred. Please try again later.', {
+              type: 'error',
+              theme: 'colored',
+            });
+          } else {
+            toast('Some error occurred', { type: 'error', theme: 'colored' });
+          }
       }
     }
 
--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,2 +1,4 @@
 export { getPaginationPages } from './getPaginationPages.ts';
 export { isErrorWithProperty } from './isErrorWithProperty.ts';
+export { isErrorWithDetailArray } from './isErrorWithDetailArray.ts';
+export { trimToMaxLength } from './trimToMaxLength.ts';
